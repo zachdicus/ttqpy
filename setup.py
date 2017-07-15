464e8e7956b34e926be9bbd67d60436fca2f229a
zachdicus <zachdicus@gmail.com>
2017-07-15 17:09:29 -0500

    Adding setup file and updating travis file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='TTQPY',
+      version='1.0',
+      description='Teacher Test/Quiz Utils',
+      author='Zach Dicus',
+      author_email='zachdicus@gmail.com',
+      url='https://github.com/zachdicus/ttqpy',
+      packages=['ttqpy']
+     )
