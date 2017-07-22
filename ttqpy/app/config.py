9b98bfb0bdc96be5a7fef879f205285833585c6f
zachdicus <zachdicus@gmail.com>
2017-07-22 13:21:32 -0500

    Change to a prod/dev db setup.

--- ttqpy/app/config.py
+++ ttqpy/app/config.py
@@ -2,20 +2,26 @@ import os
 from ttqpy.db import create_session
 import sqlite3
 
-#basedir = os.path.abspath(os.path.dirname(__file__))
+enviroment = os.getenv("TTQPY_SYSTEM", "DEVELOPMENT")
 
-#database = os.path.join(basedir, 'app.db')
+if enviroment == "DEVELOPMENT":
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    database = os.path.join(basedir, 'app.db')
+    db_uri = 'sqlite:///' + database
 
-#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + database
-SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    if not os.path.exists(database):
+        # Create the database
+        c = sqlite3.connect(database)
+        c.close()
+
+else:
+    db_uri = os.getenv("DATABASE_URL")
+
+SQLALCHEMY_DATABASE_URI = db_uri
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-#if not os.path.exists(database):
-# Create the database
-#    c = sqlite3.connect(database)
-#    c.close()
 
 # Load the tables
-#session = create_session(SQLALCHEMY_DATABASE_URI, True)
-#session.commit()
-#session.close()
+session = create_session(SQLALCHEMY_DATABASE_URI, True)
+session.commit()
+session.close()
