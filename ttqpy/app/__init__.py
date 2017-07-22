25752fe2ce12c6576b1b2a4b1e1b2b495d8439bc
zachdicus <zachdicus@gmail.com>
2017-07-22 12:57:57 -0500

    Changing the config value.

--- ttqpy/app/__init__.py
+++ ttqpy/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, g
 from ttqpy.app.views.controller import main
 from ttqpy.db import db
+
 import os
 import logging
 
@@ -11,7 +12,7 @@ app = Flask("TTQPY", root_path=root_path)
 # Register the blueprints
 app.register_blueprint(main)
 app.logger.setLevel(logging.DEBUG)
-app.config.from_object('config')
+app.config.from_object('ttqpy.app.config')
 db.init_app(app)
 
 if __name__ == "__main__":
